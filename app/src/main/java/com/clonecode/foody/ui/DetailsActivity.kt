255531b8cba3d3df4b333a9557a141b358441a55
JangHee96 <sj90947@gmail.com>
2021-05-05 16:41:40 +0900

    72. Design activity_details Layout

---
+++ app/src/main/java/com/clonecode/foody/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.clonecode.foody.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.clonecode.foody.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
