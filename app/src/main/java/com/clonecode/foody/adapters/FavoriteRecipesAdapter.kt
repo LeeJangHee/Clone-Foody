e9f19b66cec1be89ce5f524fc56d544535cda015
JangHee96 <sj90947@gmail.com>
2021-05-16 22:47:02 +0900

    101. FavoriteRecipesFragment - Long Click Listener

--- app/src/main/java/com/clonecode/foody/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/clonecode/foody/adapters/FavoriteRecipesAdapter.kt
@@ -1,17 +1,20 @@
 package com.clonecode.foody.adapters
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.*
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.clonecode.foody.R
 import com.clonecode.foody.data.database.entities.FavoritesEntity
 import com.clonecode.foody.databinding.FavoriteRecipesRowLayoutBinding
 import com.clonecode.foody.ui.fragments.favorites.FavoriteRecipesFragmentDirections
 import com.clonecode.foody.util.RecipesDiffUtil
 import kotlinx.android.synthetic.main.favorite_recipes_row_layout.view.*
 
-class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
+class FavoriteRecipesAdapter(
+    private val requireActivity: FragmentActivity
+) : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>(), ActionMode.Callback {
 
     private var favoriteRecipes = emptyList<FavoritesEntity>()
 
@@ -51,6 +54,14 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
             holder.itemView.findNavController().navigate(action)
         }
 
+        /**
+         * Long Click Listener
+         */
+        holder.itemView.favoriteRecipesRowLayout.setOnLongClickListener {
+            requireActivity.startActionMode(this)
+            true
+        }
+
     }
 
     override fun getItemCount(): Int {
@@ -63,6 +74,23 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
         val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
         favoriteRecipes = newFavoriteRecipes
         diffUtilResult.dispatchUpdatesTo(this)
+
+    }
+
+    override fun onCreateActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+        actionMode?.menuInflater?.inflate(R.menu.favorites_contextual_menu, menu)
+        return true
+    }
+
+    override fun onPrepareActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+        return true
+    }
+
+    override fun onActionItemClicked(actionMode: ActionMode?, menu: MenuItem?): Boolean {
+        return true
+    }
+
+    override fun onDestroyActionMode(actionMode: ActionMode?) {
 
     }
 }
