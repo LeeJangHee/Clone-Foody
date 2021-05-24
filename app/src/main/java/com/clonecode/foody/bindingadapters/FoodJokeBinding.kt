f885ebbb65ac35435785ac7ee803a4b270b0e9d2
JangHee96 <sj90947@gmail.com>
2021-05-24 23:43:59 +0900

    117. FoodJokeBinding class - Part 1

---
+++ app/src/main/java/com/clonecode/foody/bindingadapters/FoodJokeBinding.kt
@@ -0,0 +1,62 @@
+package com.clonecode.foody.bindingadapters
+
+import android.view.View
+import android.widget.ProgressBar
+import androidx.databinding.BindingAdapter
+import com.clonecode.foody.data.database.entities.FoodJokeEntity
+import com.clonecode.foody.models.FoodJoke
+import com.clonecode.foody.util.NetworkResult
+import com.google.android.material.card.MaterialCardView
+
+class FoodJokeBinding {
+
+    companion object {
+
+        @BindingAdapter("readApiResponse3", "readDatabase3", requireAll = false)
+        @JvmStatic
+        fun setCardAndProgressVisibility(
+            view: View,
+            apiResponse: NetworkResult<FoodJoke>?,
+            database: List<FoodJokeEntity>?
+        ) {
+            when (apiResponse) {
+                is NetworkResult.Loading -> {
+                    when (view) {
+                        is ProgressBar -> {
+                            view.visibility = View.VISIBLE
+                        }
+                        is MaterialCardView -> {
+                            view.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+                is NetworkResult.Error -> {
+                    when (view) {
+                        is ProgressBar -> {
+                            view.visibility = View.INVISIBLE
+                        }
+                        is MaterialCardView -> {
+                            view.visibility = View.VISIBLE
+                            if (database != null) {
+                                if (database.isEmpty()) {
+                                    view.visibility = View.INVISIBLE
+                                }
+                            }
+                        }
+                    }
+                }
+                is NetworkResult.Success -> {
+                    when (view) {
+                        is ProgressBar -> {
+                            view.visibility = View.INVISIBLE
+                        }
+                        is MaterialCardView -> {
+                            view.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
